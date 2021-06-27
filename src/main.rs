0f49c7e08332979a740b56633426b5dbbe862e97
Nytelife26 <xtylerjrx@gmail.com>
2021-06-27 17:18:01 +0100

    refactor(main): switch to die

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
+use die::die;
 use jukebox::Settings;
-use std::process::exit;
 
 pub fn main() {
 	let _settings = match Settings::new() {
 		Ok(set) => set,
-		Err(err) => {
-			eprintln!("Error reading configuration: {}", err);
-			exit(115);
-		}
+		Err(err) => die!("Error reading configuration: {}", err; 115),
 	};
 }
